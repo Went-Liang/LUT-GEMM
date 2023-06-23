527ea26dfcd120785e03961875b79981fdf38e18
Went-Liang <wenteng_liang@163.com>
2023-06-23 18:17:20 +0800

    add comparison with cublas

--- main.cu
+++ main.cu
@@ -4,7 +4,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <unsupported/Eigen/CXX11/Tensor>
-
+#include <cublas_v2.h>
 
 template<typename T, typename STYPE, typename BTYPE>
 void data_generator(const int q, const int g,
@@ -64,135 +64,66 @@ void data_generator(const int q, const int g,
 
     product_dims = Eigen::IndexPair<int>(1, 0);
     Y = W.contract(X, product_dims);
-
-//    std::cout << bitB << "\n";
 }
 
+class cuTimer {
+    cudaEvent_t startEvent{}, stopEvent{};
 
-template<typename T, typename STYPE, typename BTYPE>
-void checkAB(const int q, const int g,
-             const unsigned m, const unsigned n, const unsigned k,
-             const Eigen::Tensor<T, 2, Eigen::RowMajor> X, const Eigen::Tensor<T, 2, Eigen::RowMajor> Y,
-             const Eigen::Tensor<STYPE, 3, Eigen::RowMajor> A, const Eigen::Tensor<BTYPE, 3, Eigen::RowMajor> B){
-    int subgroup_size = sizeof(BTYPE) * 8;
-    Eigen::Tensor<T, 2, Eigen::RowMajor> res(m, k);
-    res.setZero();
-
-    for(int iq = 0; iq < q; ++iq){
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                for(int ik = 0; ik < k; ++ik) {
-                    int group_id = (int) ceil(j / g);
-                    STYPE a = A(i, group_id, iq);
-
-                    int subgroup_id = j / subgroup_size;
-                    BTYPE decimal_b = B(i, subgroup_id, iq);
-                    int bin_id = j % subgroup_size;
-                    BTYPE shifted = decimal_b >> bin_id;
-                    int bitValue = shifted & 1;
-                    STYPE b = (STYPE)(bitValue == 1 ? 1 : -1);
-
-                    T abx = ((T)(a * b)) * X(j, ik);
-                    res(i, ik) += abx;
-                }
-            }
-        }
+public:
+    cuTimer() {
+        cudaEventCreate(&startEvent);
+        cudaEventCreate(&stopEvent);
     }
-    Eigen::Tensor<T, 2, Eigen::RowMajor> diffArray = (res - Y).abs();
-    std::cout << "Max Error: " << diffArray.maximum() << "\n";
-}
-
-template<typename T, typename STYPE, typename BTYPE>
-void checkAB_(const int q, const int g,
-             const unsigned m, const unsigned n, const unsigned k,
-              const Eigen::Tensor<T, 2, Eigen::RowMajor> X, const Eigen::Tensor<T, 2, Eigen::RowMajor> Y,
-              const Eigen::Tensor<STYPE, 3, Eigen::RowMajor> A, const Eigen::Tensor<BTYPE, 3, Eigen::RowMajor> B){
-    int subgroup_size = sizeof(BTYPE) * 8;
-    Eigen::Tensor<T, 2, Eigen::RowMajor> res(m, k);
-    res.setZero();
-    // B -> bitB
-    // (A * B) * X
-
-    for(int iq = 0; iq < q; ++iq){
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < k; ++j){
-                for(int in = 0; in < n; ++in) {
-                    int group_id = (int) ceil(in / g);
-                    STYPE a = A(i, group_id, iq);
-
-                    int subgroup_id = in / subgroup_size;
-                    BTYPE decimal_b = B(i, subgroup_id, iq);
-                    int bin_id = in % subgroup_size;
-                    BTYPE shifted = decimal_b >> bin_id;
-                    int bitValue = shifted & 1;
-                    STYPE b = (STYPE)(bitValue == 1 ? 1 : -1);
-
-                    T abx = ((T)(a * b)) * X(in, j);
-                    res(i, j) += abx;
-                }
-            }
-        }
+    ~cuTimer() {
+        cudaEventDestroy(stopEvent);
+        cudaEventDestroy(startEvent);
     }
-    Eigen::Tensor<T, 2, Eigen::RowMajor> diffArray = (res - Y).abs();
-    std::cout << "Max Error: " << diffArray.maximum() << "\n";
-}
 
-template<typename T, typename STYPE, typename BTYPE>
-void checkAB_1(const int q, const int g,
-               const unsigned m, const unsigned n, const unsigned k,
-               const Eigen::Tensor<T, 2, Eigen::RowMajor> X, const Eigen::Tensor<T, 2, Eigen::RowMajor> Y,
-               const Eigen::Tensor<STYPE, 3, Eigen::RowMajor> A, const Eigen::Tensor<BTYPE, 3, Eigen::RowMajor> B){
-    int subgroup_size = sizeof(BTYPE) * 8;
-    Eigen::Tensor<T, 2, Eigen::RowMajor> res(m, k);
-    res.setZero();
-    // B -> bitB
-    // (A * B) * X
-    for(int i = 0; i < m; ++i){
-        for(int j = 0; j < k; ++j){
+    void start() { cudaEventRecord(startEvent); }
 
-            for(int in = 0; in < n; ++in) {
-                T w{0};
-                for(int iq = 0; iq < q; ++iq){
-                    int group_id = (int) ceil(in / g);
-                    STYPE a = A(i, group_id, iq);
-
-                    int subgroup_id = in / subgroup_size;
-                    BTYPE decimal_b = B(i, subgroup_id, iq);
-                    int bin_id = in % subgroup_size;
-                    BTYPE shifted = decimal_b >> bin_id;
-                    int bitValue = shifted & 1;
-                    STYPE b = (STYPE)(bitValue == 1 ? 1 : -1);
-                    w += a * b;
-                }
-
-                T abx = w * X(in, j);
-                res(i, j) += abx;
-            }
+    float end() {
+        cudaEventRecord(stopEvent);
+        auto error = cudaEventSynchronize(stopEvent);
+        if (error != cudaSuccess) {
+            throw std::runtime_error(cudaGetErrorString(error));
         }
+        float milliseconds = 0;
+        cudaEventElapsedTime(&milliseconds, startEvent, stopEvent);
+
+        return milliseconds;
     }
-    Eigen::Tensor<T, 2, Eigen::RowMajor> diffArray = (res - Y).abs();
-    std::cout << "Max Error: " << diffArray.maximum() << "\n";
-}
+};
 
+class gemmCuBlas {
+    cublasHandle_t handle{nullptr};
 
-template<typename T, typename STYPE, typename BTYPE>
-__global__ void lut_gemm_kernel(const int q, const int group_size,
-                                const unsigned m, const unsigned n, const unsigned k,
+public:
+    gemmCuBlas() { cublasCreate(&handle); }
+    ~gemmCuBlas() { cublasDestroy(handle); }
+
+    void operator()(const float *A, const float *B, float *C, float alpha,
+                    float beta, int M, int N, int K) const {
+        int lda = N, ldb = K, ldc = N;
+        cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha, B, lda, A,
+                    ldb, &beta, C, ldc);
+    }
+};
+
+template<typename T, typename STYPE, typename BTYPE, int group_size, int q>
+__global__ void lut_gemm_kernel(const unsigned m, const unsigned n, const unsigned k,
                                 const unsigned A_col_num, const unsigned B_col_num,
-                                const STYPE *A, const BTYPE *B, const T *X,
-                                T *Y) {
-// grid(m/th, n/g), block(th)
-// W(m*n), X(n*k), Y(m*k), AB(m*n*q)
-// m = 2, n = 8, g = 8,
+                                const STYPE *__restrict__ A, const BTYPE *__restrict__ B, const T *__restrict__ X,
+                                T *__restrict__ Y) {
     constexpr unsigned int subgroup_size = sizeof(BTYPE) * 8;
-    unsigned int lut_num = group_size / subgroup_size;
-    unsigned int lut_size = pow(2, subgroup_size);
+    constexpr unsigned int lut_num = group_size / subgroup_size;
+    const unsigned int lut_size = pow(2, subgroup_size);
     unsigned int group_id = blockIdx.y;
     unsigned int row_id = threadIdx.x + blockDim.x * blockIdx.x;
     extern __shared__ T luts[];
 
     for(int ik = 0; ik < k; ++ik) {
         if (threadIdx.x < lut_size) {
+#pragma unroll
             for (int i = 0; i < lut_num; ++i) {
                 T sum = 0;
 #pragma unroll
@@ -207,6 +138,7 @@ __global__ void lut_gemm_kernel(const int q, const int group_size,
 
         for (int iq = 0; iq < q; ++iq) {
             T sum = 0;
+#pragma unroll
             for (int i = 0; i < lut_num; ++i) {
                 BTYPE decimal_b = B[row_id * B_col_num * q + (group_id * lut_num + i) * q + iq];
                 sum += luts[i * lut_size + decimal_b];
@@ -227,9 +159,10 @@ int main() {
     int fp32CoresNum = 640;
     double peakPerformance = boostFrequency * fp32CoresNum * 2;
     printf("clock rate %.3f GHz, FP32 cores num %d, FP32 peak throughput %.3f "
-            "GFLOPS\n",
-            boostFrequency, fp32CoresNum, peakPerformance);
+           "GFLOPS\n",
+           boostFrequency, fp32CoresNum, peakPerformance);
     omp_set_num_threads(omp_get_num_procs());
+    int iteration = 10;
 
 
 //    typedef Eigen::bfloat16 T;        // W X Y: before quant
@@ -237,68 +170,91 @@ int main() {
     typedef float STYPE;                // scaleMat  A
     typedef std::uint8_t BTYPE;         // binaryMat B
     int subgroup_size = sizeof(BTYPE) * 8;
-    int q = 3;
-    int g = 128;
-    unsigned m = 4096;
+    const int q = 3;
+    const int g = 128;
+    unsigned m = 2048;
     unsigned n = 1024;
-    unsigned k = 8;
-    unsigned A_col_num = (int)ceil((float)n / (float)g);
-    unsigned B_col_num = (int)ceil((float)n / (float)subgroup_size);
+    unsigned k = 1;
+    unsigned A_col_num = (int) ceil((float) n / (float) g);
+    unsigned B_col_num = (int) ceil((float) n / (float) subgroup_size);
 
     Eigen::Tensor<T, 2, Eigen::RowMajor> W(m, n), X(n, k), Y(m, k);
     Eigen::Tensor<STYPE, 3, Eigen::RowMajor> A(m, A_col_num, q);
     Eigen::Tensor<BTYPE, 3, Eigen::RowMajor> B(m, B_col_num, q);
-
     data_generator<T, STYPE, BTYPE>(q, g, m, n, k,
                                     W, X, Y, A, B);
-    checkAB_1<T, STYPE, BTYPE>(q, g, m, n, k,
-                               X, Y, A, B);
 
 
     STYPE *deviceAPtr;
     BTYPE *deviceBPtr;
+    T *deviceWPtr;
     T *deviceXPtr;
     T *deviceYPtr;
     cudaMalloc(&deviceAPtr, (m * A_col_num * q) * sizeof(STYPE));
     cudaMalloc(&deviceBPtr, (m * B_col_num * q) * sizeof(BTYPE));
+    cudaMalloc(&deviceWPtr, (m * n) * sizeof(T));
     cudaMalloc(&deviceXPtr, (n * k) * sizeof(T));
     cudaMalloc(&deviceYPtr, (m * k) * sizeof(T));
     cudaMemcpy(deviceAPtr, A.data(), (m * A_col_num * q) * sizeof(STYPE),
                cudaMemcpyHostToDevice);
     cudaMemcpy(deviceBPtr, B.data(), (m * B_col_num * q) * sizeof(BTYPE),
                cudaMemcpyHostToDevice);
+    cudaMemcpy(deviceWPtr, W.data(), (m * n) * sizeof(T),
+               cudaMemcpyHostToDevice);
     cudaMemcpy(deviceXPtr, X.data(), (n * k) * sizeof(T),
                cudaMemcpyHostToDevice);
-    cudaMemset(deviceYPtr, 0, (m * k) * sizeof(T));
     Eigen::Tensor<T, 2, Eigen::RowMajor> cuY(m, k);
 
-    int th = 1024;
-    dim3 block(th);
-    dim3 grid((m - 1) / block.x + 1, (n - 1) / g + 1);
+    cuTimer timer{};
 
-    lut_gemm_kernel<T, STYPE, BTYPE>
+    {
+        int th = 1024;
+        dim3 block(th);
+        dim3 grid((m - 1) / block.x + 1, (n - 1) / g + 1);
+        cudaMemset(deviceYPtr, 0, (m * k) * sizeof(T));
+        lut_gemm_kernel<T, STYPE, BTYPE, g, q>
+        <<<grid, block, pow(2, subgroup_size) * (g / subgroup_size) * sizeof(T)>>>
+                (m, n, k,
+                 A_col_num, B_col_num,
+                 deviceAPtr, deviceBPtr, deviceXPtr, deviceYPtr);
+
+        cudaDeviceSynchronize();
+        cudaMemcpy(cuY.data(), deviceYPtr, (m * k) * sizeof(T), cudaMemcpyDeviceToHost);
+        Eigen::Tensor<T, 2, Eigen::RowMajor> diffArray = (cuY - Y).abs();
+        std::cout << "lut-gemm Max Error: " << diffArray.maximum() << " ";
+
+        double elapsedTime = 0;
+        for (int i = 0; i < iteration; ++i) {
+            cudaMemset(deviceYPtr, 0, (m * k) * sizeof(T));
+            timer.start();
+            lut_gemm_kernel<T, STYPE, BTYPE, g, q>
             <<<grid, block, pow(2, subgroup_size) * (g / subgroup_size) * sizeof(T)>>>
-            (q, g, m, n, k,
-             A_col_num, B_col_num,
-             deviceAPtr, deviceBPtr, deviceXPtr, deviceYPtr);
+                    (m, n, k,
+                     A_col_num, B_col_num,
+                     deviceAPtr, deviceBPtr, deviceXPtr, deviceYPtr);
+            elapsedTime += timer.end();
+        }
+        elapsedTime /= iteration;
+        printf("Average Time: %.3f ms\n", elapsedTime);
+    }
 
-    cudaDeviceSynchronize();
-    cudaMemcpy(cuY.data(), deviceYPtr, (m * k) * sizeof(T), cudaMemcpyDeviceToHost);
-    Eigen::Tensor<T, 2, Eigen::RowMajor> diffArray = (cuY - Y).abs();
-    std::cout << "lut-gemm Max Error: " << diffArray.maximum() << "\n";
+    {
+        cudaMemset(deviceYPtr, 0, (m * k) * sizeof(T));
+        gemmCuBlas cublas_gemm;
+        cublas_gemm(deviceWPtr, deviceXPtr, deviceYPtr, 1.0, 0.0, m, k, n);
+        cudaDeviceSynchronize();
+        cudaMemcpy(cuY.data(), deviceYPtr, (m * k) * sizeof(T), cudaMemcpyDeviceToHost);
+        Eigen::Tensor<T, 2, Eigen::RowMajor> diffArray = (cuY - Y).abs();
+        std::cout << "cublas-gemm Max Error: " << diffArray.maximum() << ", ";
 
-
-//    std::cout << W << std::endl;
-//    std::cout << "\n";
-//    std::cout << X << std::endl;
-//    std::cout << "\n";
-//    std::cout << Y << std::endl;
-//    std::cout << "\n";
-//    std::cout << A << std::endl;
-//    std::cout << "\n";
-//    std::cout << B << std::endl;
-//    std::cout << "\n";
-//    std::cout << cuY << std::endl;
-
-
+        double elapsedTime = 0;
+        for (int i = 0; i < iteration; ++i) {
+            cudaMemset(deviceYPtr, 0, (m * k) * sizeof(T));
+            timer.start();
+            cublas_gemm(deviceWPtr, deviceXPtr, deviceYPtr, 1.0, 0.0, m, k, n);
+            elapsedTime += timer.end();
+        }
+        elapsedTime /= iteration;
+        printf("Average Time: %.3f ms\n", elapsedTime);
+    }
 }
